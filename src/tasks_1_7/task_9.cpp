58a4d6824f51ee91895e66fe6017886a088a3dc5
Aldarov <alex_aldarov@mail.ru>
2022-10-21 23:35:52 +0800

    fix

--- src/tasks_1_7/task_9.cpp
+++ src/tasks_1_7/task_9.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-float avg(int *values, int size)
+float avg(vector<int> values)
 {
-    if (size == 0)
+    if (values.empty())
         return 0;
 
     int sum = 0;
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < values.size(); i++)
     {
-        cout << values[i] << "\n";
         sum += values[i];
     }
 
-    return sum / size;
+    auto const count = static_cast<float>(values.size());
+
+    return sum / count;
 }
 
-class Student
+struct Student
 {
     public:
         string fullName;
 
         string group;
 
-        int marks[5];
+        vector<int> marks;
 
         float avgMark()
         {
-            // int size = sizeof(masks) / sizeof(masks[0]);
-            return avg(marks, 5);
+            return avg(marks);
         }
 
         bool isAllMarks(int mark)
@@ -47,36 +48,46 @@ class Student
             return res;
         }
 
-        Student(string _fullName, string _group, int _marks[5])
+        Student(string _fullName, string _group, vector<int> _marks)
         {
             fullName = _fullName;
             group = _group;
-            auto &marks = _marks;
+            marks = _marks;
         }
-
-        // Student(string _fullName, string _group)
-        // {
-        //     fullName = _fullName;
-        //     group = _group;
-        // }
 };
 
 vector<Student> initStudents()
 {
     vector<Student> res;
 
-    res.push_back(Student("Иванов ИИ", "05111", new int[5]{ 1, 2, 3, 4, 5 }));
-    res.push_back(Student("Петров АА", "05121", new int[5]{ 4, 4, 4, 4, 4 }));
-    res.push_back(Student("Сидоров ББ", "05311", new int[5]{ 5, 5, 5, 5, 5 }));
-    res.push_back(Student("Иванов2 ИИ", "05111", new int[5]{ 1, 2, 3, 4, 5 }));
-    res.push_back(Student("Петров2 АА", "05121", new int[5]{ 4, 4, 4, 4, 4 }));
-    res.push_back(Student("Сидоров2 ББ", "05311", new int[5]{ 5, 5, 5, 5, 5 }));
-    res.push_back(Student("Иванов3 ИИ", "05111", new int[5]{ 1, 2, 3, 4, 5 }));
-    res.push_back(Student("Петров3 АА", "05121", new int[5]{ 4, 4, 4, 4, 4 }));
-    res.push_back(Student("Сидоров3 ББ", "05311", new int[5]{ 5, 5, 5, 5, 5 }));
-    res.push_back(Student("Сидоров4 ББ", "05311", new int[5]{ 5, 5, 5, 5, 5 }));
+    res.push_back(Student("Иванов ИИ", "05111", vector<int> { 1, 2, 3, 4, 5 }));
+    res.push_back(Student("Петров АА", "05121", vector<int> { 4, 4, 4, 4, 4 }));
+    res.push_back(Student("Сидоров ББ", "05311", vector<int> { 5, 5, 5, 5, 5 }));
+    res.push_back(Student("Иванов2 ИИ", "05111", vector<int> { 1, 2, 3, 4, 5 }));
+    res.push_back(Student("Петров2 АА", "05121", vector<int> { 4, 4, 4, 4, 4 }));
+    res.push_back(Student("Сидоров2 ББ", "05311", vector<int> { 5, 5, 5, 5, 5 }));
+    res.push_back(Student("Иванов3 ИИ", "05111", vector<int> { 2, 2, 2, 2, 2 }));
+    res.push_back(Student("Петров3 АА", "05121", vector<int> { 4, 4, 4, 4, 4 }));
+    res.push_back(Student("Сидоров3 ББ", "05311", vector<int> { 5, 5, 5, 5, 5 }));
+    res.push_back(Student("Сидоров4 ББ", "05311", vector<int> { 5, 5, 5, 5, 5 }));
 
     return res;
+}
+
+struct {
+    bool operator() (Student a, Student b) const
+    {
+        return a.avgMark() < b.avgMark();
+    }
+} StudentLess;
+
+void showStudentsOnlyWith45(vector<Student> students)
+{
+  for (int i = 0; i < students.size(); i++)
+  {
+    if (students[i].isAllMarks(4) or students[i].isAllMarks(5))
+        cout << students[i].fullName << " - " << students[i].group << " гр.\n";
+  }
 }
 
 int main()
@@ -84,6 +95,10 @@ int main()
     setlocale(LC_ALL, "");
 
     vector<Student> students = initStudents();
+
+    sort(students.begin(), students.end(), StudentLess);
+
+    showStudentsOnlyWith45(students);
 
     return 0;
 }
